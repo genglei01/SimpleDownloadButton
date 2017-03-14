584d90bcd5f9c2e048738ce1aeb742c41af50a2b
LeoGeng <leo.geng@grapecity.com>
2017-03-14 15:25:56 +0800

    Initial Commit

---
+++ GLSimpleDownloadButton/GLSimpleDownloadButton.h
@@ -0,0 +1,19 @@
+//
+//  GLSimpleDownloadButton.h
+//  GLSimpleDownloadButton
+//
+//  Created by LeoGeng on 14/03/2017.
+//  Copyright © 2017 LeoGeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GLSimpleDownloadButton.
+FOUNDATION_EXPORT double GLSimpleDownloadButtonVersionNumber;
+
+//! Project version string for GLSimpleDownloadButton.
+FOUNDATION_EXPORT const unsigned char GLSimpleDownloadButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLSimpleDownloadButton/PublicHeader.h>
+
+
